c98dfb3e7c23fdf41c98f168b15a218c31b4f8b2
Salah <mosalahkd028@gmail.com>
2026-01-26 07:16:21 -0700

    fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { SiteHeader } from "@/components/layout/SiteHeader";
 import { ChefSection } from "@/components/sections/ChefSection";
 import { HeroSection } from "@/components/sections/HeroSection";
 import { MapSection } from "@/components/sections/MapSection";
-import { Gallery } from "@/components/@/components/sections/Gallery";
+import { Gallery } from "@/components/sections/Gallery";
 import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
 import { About } from "@/components/sections/About";
 
