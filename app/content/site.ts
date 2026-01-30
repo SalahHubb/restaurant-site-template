6aedf6c5f080047a055e7883eff59738bb9d77cb
Salah <mosalahkd028@gmail.com>
2026-01-30 05:54:14 -0700

    add content site for reem

--- app/content/site.ts
+++ app/content/site.ts
@@ -20,7 +20,7 @@ export type SimpleLink = { label: string; href: string };
 
 export const siteData = {
   business: {
-    brand: { name: "AL BAIK", icon: "\uD83C\uDF5C" },
+    brand: { name: "Reem Restaurant", icon: "\uD83C\uDF5C" },
     navLinks: [
       { label: "Home", href: "#" },
       { label: "About", href: "#about" },
@@ -165,8 +165,8 @@ export const siteData = {
   },
   contact: {
     href: "#contact",
-    phone: "0960771111",
-    address: "Adama 11 kebele Nearby Kitur 4 School, Adama",
+    phone: "0930033877",
+    address: "Hayahulet, Addis Ababa",
   },
   social: [
     { label: "Facebook", href: "#", Icon: Facebook },
@@ -174,9 +174,9 @@ export const siteData = {
     { label: "Twitter", href: "#", Icon: Twitter },
   ] as SocialLink[],
   map: {
-    heading: "Adama 11 kebele Nearby  ",
-    address: "Kitur 4 School, Adama",
+    heading: "Equatorial Guinea St, Addis Ababa ",
+    address: "Hayahulet",
     embedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15782.29166593225!2d39.26864934539101!3d8.540775683900337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b21fe2c5899ab%3A0x353cc0159cbdef1!2sAL%20BAIK%20ADAMA%20RESTAURANT!5e0!3m2!1sen!2set!4v1769612456003!5m2!1sen!2set",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.5165895400023!2d38.79125907480135!3d9.01654968918417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85f51c92f01f%3A0x9678d26d78d90d08!2zUmVlbSBSZXN0YXVyYW50IHwgSGF5YWh1bGV0IHwg4Yiq4YidIOGIrOGIteGJtuGIq-GKleGJtSB8IOGIg-GLq-GIgeGIiOGJtQ!5e0!3m2!1sen!2set!4v1769777006050!5m2!1sen!2set",
   },
 };
